72c6b0611fcd0a264d439262e1beeebc364a633e
jeremy11006 <jeremy11006@users.noreply.github.com>
2017-04-22 23:35:27 +0800

    Add files via upload
    just test

---
+++ 清除IE_tomcat缓存.bat
@@ -0,0 +1,5 @@
+del /f /s /q "C:\Users\Administrator\AppData\Local\Microsoft\Windows\Temporary Internet Files\*.*"
+
+rd /s/q "D:\apache\apache-tomcat-7.0.59\work\Catalina"
+
+rd /s/q "D:\apache\apache-tomcat-6.0.37\work\Catalina"
